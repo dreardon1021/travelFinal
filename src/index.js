327d260386a5d5c59f705fb94a5cc198f6c2ec24
Dan Reardon <dreardon1021@gmail.com>
2020-02-26 16:16:53 -0700

    Remove scripts import due to deletion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-import './scripts'
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
